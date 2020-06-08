ecc71f2b5387b87d3f452723fc18b76c172788a1
hansaemgo <rhgkstoa7@gmail.com>
2020-06-08 17:52:12 +0900

    weather  API

--- weather.js
+++ weather.js
@@ -1,5 +1,24 @@
+const weatherContainer = document.querySelector('.js-weather');
+const weather = weatherContainer.querySelector('h3');
 const API_KEY = 'ec757e904841990307f2d7eed9e63a50';
+
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+	)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (json) {
+			console.log(json);
+			const temperature = json.main.temp;
+			const place = json.name;
+			weather.innerText = `${temperature} @ ${place}`;
+			weather.classList.add('weatherTitle');
+		});
+}
 
 function saveCoords(coordsObj) {
 	localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +33,7 @@ function handleGeoSucces(position) {
 		longitude,
 	};
 	saveCoords(coordsObj);
+	getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -24,11 +44,13 @@ function askForCoords() {
 }
 
 function loadCoords() {
-	const loadedCords = localStorage.getItem(COORDS);
-	if (loadedCords === null) {
+	const loadedCoords = localStorage.getItem(COORDS);
+	if (loadedCoords === null) {
 		askForCoords();
 	} else {
-		//getWeather
+		const parseCoords = JSON.parse(loadedCoords);
+		console.log(parseCoords);
+		getWeather(parseCoords.latitude, parseCoords.longitude);
 	}
 }
 
